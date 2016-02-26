404973812ffa0a1db3d96f9327d6fcc31abe8693
Vania <vania.baek@gmail.com>
2016-02-25 21:56:43 -0800

    Changing to port 80

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http')
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, this is a future home for Bud the Pomeranian\n');
