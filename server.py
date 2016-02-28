a4a2d6bd5293727898d95e36e1e136e509096cb6
Vania <vania.baek@gmail.com>
2016-02-28 14:33:10 -0800

    Change port configuration to port 5000

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__) #this feeds the name of the file to Flask 
 
 @app.route("/")
 def hello():
 	return "Hello World!"
 
 if __name__ == '__main__':
-	app.run()
-
+	app.run(host='0.0.0.0', port=5000)
 	
