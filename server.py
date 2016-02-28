9dc7708abb2ad944f720472882a134ef969c04da
Vania <vania.baek@gmail.com>
2016-02-28 14:42:07 -0800

    Added html file

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__) #this feeds the name of the file to Flask 
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
-	
+
