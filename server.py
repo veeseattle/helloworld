8a3c0ed4c18d5aaebc403812b2be5847290453f9
Vania <vania.baek@gmail.com>
2016-02-29 13:27:50 -0800

    Changes

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ def hello():
 	with open('data.csv', 'r') as csvfile:
 		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 		fieldnames = ('total', 'used', 'free', 'shared', 'buffers')
-		return render_template('template.html', my_string=csvreader, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
+		dataList = list(csvreader)
+		return render_template('template.html', my_string=fieldnames, my_list=dataList)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
