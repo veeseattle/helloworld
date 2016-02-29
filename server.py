232be674b4f6ed4eca250c202e8899d25a0d3794
Vania <vania.baek@gmail.com>
2016-02-29 09:52:51 -0800

    Changes

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ def hello():
 		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 		for row in csvreader:
 			print ' '.join(row)
-	return render_template('template.html', my_string="My Data", my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
+	return render_template('template.html', my_string=csvreader, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
