a6b6387f0fcae9f73650e6a935d1833c569c4e5e
Vania <vania.baek@gmail.com>
2016-02-29 09:41:42 -0800

    Fixed csv

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, csv
 app = Flask(__name__) #this feeds the name of the file to Flask 
 
 @app.route("/")
 def hello():
-	f = open('data', 'r')
-	read_data = f.read()
-	return render_template('template.html', my_string=read_data, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
+	with open('data.csv', 'r') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		for row in csvreader:
+			print ', '.join(row)
+	return render_template('template.html', my_string="My Data", my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
