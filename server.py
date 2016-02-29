ef21d660bd1960b62fd50b42e9d22f5cbe451bad
Vania <vania.baek@gmail.com>
2016-02-29 13:32:34 -0800

    Changes

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__) #this feeds the name of the file to Flask
 def hello():
 	with open('data.csv', 'r') as csvfile:
 		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-		fieldnames = ('total', 'used', 'free', 'shared', 'buffers')
+		fieldnames = 'total, used, free, shared, buffers'
 		dataList = list(csvreader)
 		return render_template('template.html', my_string=fieldnames, my_list=dataList)
 
