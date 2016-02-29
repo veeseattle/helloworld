ecac05c197320a1d5e7e068239048e6da1cbe37a
Vania <vania.baek@gmail.com>
2016-02-29 14:03:20 -0800

    Changes

--- server.py
+++ server.py
@@ -4,12 +4,14 @@ import csv
 app = Flask(__name__) #this feeds the name of the file to Flask 
 
 @app.route("/")
-def hello():
+def show_mem_table():
 	with open('data.csv', 'r') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 		fieldnames = 'total, used, free, shared, buffers'
-		dataList = list(csvreader)
-		return render_template('template.html', my_string=fieldnames, my_list=dataList)
+		values = []
+		for value in csvreader:
+			values = '\t'.join(value)
+		return render_template('template.html', my_string=fieldnames, my_list=values)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
