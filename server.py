26ec22ae855cdf285fbd899fea74942d742b9a5d
Vania <vania.baek@gmail.com>
2016-02-29 09:29:51 -0800

    Tried adding shit

--- server.py
+++ server.py
@@ -3,8 +3,11 @@ app = Flask(__name__) #this feeds the name of the file to Flask
 
 @app.route("/")
 def hello():
-	return render_template('template.html', my_string="Task Manager", my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
+	f = open('data', 'r')
+	read_data = f.read()
+	return render_template('template.html', my_string=read_data, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
+
 
