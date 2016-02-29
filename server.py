7259db4b8e612bea4aa506c477322151b9ca07a5
Vania <vania.baek@gmail.com>
2016-02-29 13:21:45 -0800

    Changes

--- server.py
+++ server.py
@@ -7,9 +7,8 @@ app = Flask(__name__) #this feeds the name of the file to Flask
 def hello():
 	with open('data.csv', 'r') as csvfile:
 		csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-		try: 
-			fieldnames = ('total', 'used', 'free', 'shared', 'buffers')
-	return render_template('template.html', my_string=csvreader, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
+		fieldnames = ('total', 'used', 'free', 'shared', 'buffers')
+		return render_template('template.html', my_string=csvreader, my_list=["Memory Free","Compressed Mem","Ports","PID","User"])
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
