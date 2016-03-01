326657a70ad554d785c635c8b540c061e0b11fb5
Ubuntu <bud@budsubuntu.bhjtau54jxmunpo0tup4xft0qc.bx.internal.cloudapp.net>
2016-03-01 07:26:00 +0000

    changes

--- server.py
+++ server.py
@@ -5,13 +5,16 @@ app = Flask(__name__) #this feeds the name of the file to Flask
 
 @app.route("/")
 def show_mem_table():
-	with open('data.csv', 'r') as csvfile:
-		csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		memheaders = ['total, used, free, shared, buffers']
-		values = []
-		for value in csvreader:
-			values.append(value)
-		return render_template('template.html', my_string=memheaders, my_list=values)
+	try:
+		with open('data.csv', 'r') as csvfile:
+			csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+			memheaders = ['total, used, free, shared, buffers']
+			values = []
+			for value in csvreader:
+				values.append(value)
+			return render_template('template.html', my_string=memheaders, my_list=values)
+	except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
